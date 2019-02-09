e7eac895f655fcee553fd5f623542166809a9b72
Juraji <robin@juraji.nl>
2019-02-09 21:01:19 +0100

    - public_api.ts: Add all decorators to public export - Remove default root project - angular.json: Set decorators lib as default project

--- projects/decorators/src/public_api.ts
+++ projects/decorators/src/public_api.ts
@@ -2,5 +2,12 @@
  * Public API Surface of decorators
  */
 
+// Debounce
 export * from "./lib/debounce/debounce";
+export * from "./lib/debounce/debounce-until";
+
+// Logging
+export * from "./lib/logging/log-me";
+
+// Query parameter binding
 export * from "./lib/query-parameter-binding/query-parameter-binding";
